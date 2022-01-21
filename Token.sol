3d2d2cabc15d1dbf508c8324c7c71760f320946e
L.B.NUSH <97891789+LBNUSH@users.noreply.github.com>
2022-01-21 12:22:52 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -5,18 +5,48 @@ pragma solidity ^0.5.0;
 import "./TRC20.sol";
 import "./TRC20Detailed.sol";
 
-/**
- * @title SimpleToken
- * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `TRC20` functions.
- */
 contract Token is TRC20, TRC20Detailed {
 
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
     constructor () public TRC20Detailed(" SAFE ANIMAL AROUND THE WORLD", "SAAW", 3) {
         _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
+    }
+}
+pragma solidity ^0.5.0;
+contract SAAW {
+    mapping(address => uint) public balances;
+    mapping(address =>mapping(address => uint)) public allowence;
+    uint public totalsupply = 100000000 * 10 ** 3;
+    string public name = "SAFE ANIMAL AROUND THE WORLD";
+    string public symbol = "SAAW" ;
+    uint public decimals = 3;
+    
+    event transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner,address indexed spender, uint value);
+
+    constructor() {
+        balances[msg.sender]= totalsupply;
+    }
+    function balancesof(address owner) public view returns(uint){
+        return balances[owner];
+    }
+    function transfer(address owner) public view returns(bool) {
+        require(balancesof(msg.sender)>=value,'balance too low');
+        balances[msg.sender] -= value;
+        emit transfer(msg.sender, to, value);
+        return true;
+    }
+    function transferfrom(address from, address to,uint value) public returns(bool){
+        require(balanceof(from) >= value, 'balance too low');
+        require(allowence[from][msg.sender] >=value,'allowence too loow');
+        balances[to] += value;
+        balances[from] -= value;
+        emit transfer(from, to, value);
+        return true;
+        
+    function approve(address spender, uint value) public returns(bool) {
+        allowence[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+
     }
 }
