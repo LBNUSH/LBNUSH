244c2265fd9b60abfd10eb70fcd469a2ae00e058
L.B.NUSH <97891789+LBNUSH@users.noreply.github.com>
2022-01-21 14:21:26 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -2,13 +2,22 @@
 // Enable optimization
 pragma solidity ^0.5.0;
 
-contract Token {
+import "./TRC20.sol";
+import "./TRC20Detailed.sol"; {
 
-    mapping(address => uint) public balances;
-    mapping(address =>mapping(address => uint)) public allowence;
-    uint public totalsupply = 100000000;
-    string public name = "SAFE ANIMAL AROUND THE WORLD";
-    string public symbol = "SAAW" ;
-    uint public decimals = 3;
-    
+/**
+ * @title SimpleToken
+ * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `TRC20` functions.
+ */
+contract Token is TRC20, TRC20Detailed {
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public TRC20Detailed("SAFE ANIMAL AROUND THE WORLD", "SAAW", 3) {
+        _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
+    }
 }
+
